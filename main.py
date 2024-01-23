84001dbffe207ed4564ccf2fa8491678b138deab
caydenwelter <caydenwelter@gmail.com>
2024-01-23 13:18:39 -0600

    update: moved code to scraper.py

--- main.py
+++ main.py
@@ -1,24 +1,6 @@
 def main():
-    import requests
-    import re
-    import json
-    from bs4 import BeautifulSoup
-    from time import gmtime, strftime
-    
-    url = "https://recwell.wisc.edu/liveusage/"
-    response = requests.get(url)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.content, 'html5lib')
-
-    trackers = soup.find_all("div", {"class": "live-tracker"})
-    # print(trackers)
-
-    data_dict = {}
-    for tracker in trackers:
-        location = tracker.find_all("div", {"class": "tracker-location"})
-        if location not in data_dict.keys():
-            data_dict[location] = []
-        # strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    import scraper
+    scraper.collect()
         
 
 if __name__ == "__main__":
